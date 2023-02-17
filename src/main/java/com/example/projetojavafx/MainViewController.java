0f1594b164d26331d30b3ad72c2e7cc2daf3cd6b
Rhama Krisner <rhamakrisner@gmail.com>
2023-02-17 14:23:09 -0300

    Adicionado a tela About no root do ScrollPane

--- src/main/java/com/example/projetojavafx/MainViewController.java
+++ src/main/java/com/example/projetojavafx/MainViewController.java
@@ -1,9 +1,17 @@
 package com.example.projetojavafx;
 
+import com.example.projetojavafx.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,11 +37,31 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemAboutAction(){
-        System.out.println("onMenuItemAboutAction");
+        loadView("About.fxml");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    private void loadView(String absoluteName){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = MainApplication.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+
+
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error Loading View",
+                    e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
